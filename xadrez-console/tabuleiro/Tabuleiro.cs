5438e8a913a38e50f0e61db1a877fd99d57d572c
João <joaovictormdasilva676@gmail.com>
2021-05-26 22:40:00 -0300

    Comecando a implementar um metodo que impreme o tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -6,11 +6,17 @@
         public int colunas { get; set; }
         private Peca[,] pecas;
 
-        public Tabuleiro(int linha, int colunas)
+        public Tabuleiro(int linhas, int colunas)
         {
-            this.linhas = linha;
+            this.linhas = linhas;
             this.colunas = colunas;
-            pecas = new Peca[linha, colunas];
+            pecas = new Peca[linhas, colunas];
         }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+        
     }
 }
